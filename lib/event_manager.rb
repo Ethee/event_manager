c05ed9869041e57660536ca614258eb994946fe1
Ethee <admin@ethee.dev>
2022-02-15 21:43:32 -0500

    Initial commit, most of program complete, final assignment left

---
+++ lib/event_manager.rb
@@ -0,0 +1,76 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+require 'date'
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def clean_phone_number(phone_number)
+  phone_number = phone_number.to_s.delete "()-. "
+  if phone_number.length === 10
+    puts "GOOD!"
+    return phone_number
+  elsif phone_number.length === 11 && phone_number[0] === '1'
+    puts "GOOD!"
+    return phone_number
+  else
+    puts "BAD!"
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+def time_target(date_time)
+  date_time = Time.parse(date_time)
+  date_time.hour
+
+end
+
+puts 'Event Manager Initialized!'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  date = row[:regdate]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  phone_number = clean_phone_number(row[:homephone])
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
+end
